d66b8a27a41d941bb31a2ee88827a2c7ace860e7
NCalhoun31 <ncalhoun@uark.edu>
2025-02-04 14:41:44 -0600

    Added main.py
    Added main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+class Main:
+
+    #default constructor
+    #add member variables as needed
+    def __init__(self):
+        pass
+
+    #update any member variables here
+    def update(self):
+        pass
+    
+
+#declare main object
+m = Main()
+
+#create main loop
+running = True
+while running:
+
+    #temporary code until we get the GUI setup
+    userValue = input("Enter q to leave \n")
+
+    if userValue == "q":
+        running = False
+
+    m.update()
