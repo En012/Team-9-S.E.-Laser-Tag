906fa493d225fc3d325c320c208a735c5c637c56
trevman00 <trevorr@uark.edu>
2025-02-04 15:35:48 -0600

    Adding Splash Screen(General)

---
+++ screen.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import os
+from PIL import Image, ImageTk
+
+root = tk.Tk()
+root.title("Image Display")
+root.configure(bg="black")
+
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+
+img_path = os.path.expanduser("~/Downloads/logo.jpg")
+def switch():
+    global img, label, height, width
+    img_path = os.path.expanduser("~/Downloads/lebron.jpg")
+    img = Image.open(img_path)
+    img = img.resize((width, height), Image.LANCZOS)
+    img = ImageTk.PhotoImage(img)
+    label.configure(image=img)
+
+
+img = Image.open(img_path)
+img = img.resize((width, height), Image.LANCZOS)
+img = ImageTk.PhotoImage(img)
+label = tk.Label(root, image=img, bg="black")    
+label.pack()
+root.after(3000, switch)
+root.mainloop()
