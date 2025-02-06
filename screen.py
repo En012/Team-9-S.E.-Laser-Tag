aaf624ba6ba630f9d7f289a5d39412d137e0daa8
NCalhoun31 <ncalhoun@uark.edu>
2025-02-06 14:11:25 -0600

    Changed file path in screen.py

--- screen.py
+++ screen.py
@@ -9,10 +9,10 @@ root.configure(bg="black")
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
 
-img_path = os.path.expanduser("~/Downloads/logo.jpg")
+img_path = os.path.expanduser("/images/logo.jpg")
 def switch():
     global img, label, height, width
-    img_path = os.path.expanduser("~/Downloads/lebron.jpg")
+    img_path = os.path.expanduser("/images/lebron.jpg")
     img = Image.open(img_path)
     img = img.resize((width, height), Image.LANCZOS)
     img = ImageTk.PhotoImage(img)
