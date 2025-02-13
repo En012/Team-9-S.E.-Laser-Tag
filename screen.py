4ab7b0e60134198bec51726034d11eb7c5822a98
NCalhoun31 <ncalhoun@uark.edu>
2025-02-13 14:58:42 -0600

    Made screen.py to work with main.py

--- screen.py
+++ screen.py
@@ -4,22 +4,22 @@ import os
 import playerentryscreentest2 as pe
 from PIL import Image, ImageTk
 
-root = tk.Tk()
-root.title("Image Display")
-root.configure(bg="black")
+class Screen:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Image Display")
+        self.root.configure(bg="black")
 
-width = root.winfo_screenwidth()
-height = root.winfo_screenheight()
+        self.width = self.root.winfo_screenwidth()
+        self.height = self.root.winfo_screenheight()
 
-img_path = os.path.expanduser("images/logo.jpg")
-def switch():
-    pe.player_entry_screen(root)
+        img_path = os.path.expanduser("images/logo.jpg")
+        self.img = Image.open(img_path)
+        self.img = self.img.resize((self.width, self.height), Image.LANCZOS)
+        self.img = ImageTk.PhotoImage(self.img)
+        self.label = tk.Label(self.root, image=self.img, bg="black")
+        self.label.pack()
+        self.root.after(3000, self.switch)
 
-
-img = Image.open(img_path)
-img = img.resize((width, height), Image.LANCZOS)
-img = ImageTk.PhotoImage(img)
-label = tk.Label(root, image=img, bg="black")    
-label.pack()
-root.after(3000, switch)
-root.mainloop()
+    def switch(self):
+        pe.player_entry_screen(self.root)
