a909d27576d3e75e9eef956020b8e483f2bd5a2e
NCalhoun31 <ncalhoun@uark.edu>
2025-03-06 20:01:14 -0600

    test

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from display import Display
 #initalize the database
 database.initialize_database()
 
+print("test")
+
 # Main function
 if __name__ == "__main__":
     #this line starts up the udp server upon application start (if not here, server is not running until address swap)
